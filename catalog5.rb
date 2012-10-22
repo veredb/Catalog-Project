f28e0f0926b43ca8ff00d0c31b3c9abe8e7eee9a
vered bauer <veredbr2000@yahoo.com>
2012-10-22 12:19:53 -0500

    Updated catalog5.rb

--- catalog5.rb
+++ catalog5.rb
@@ -1,5 +1,5 @@
 require 'csv'
-require 'pry'
+
 class Element
 def initialize(name, symbol, num, weight, state, type)
       @name = name
@@ -24,11 +24,6 @@ def to_s
                  str += @type.to_s.ljust(20)
        str
 
-end
-
-def to_array
-   
-      [@name, @symbol, @num, @weight, @state, @type]
 end
 
 end    #end of class Element
@@ -66,25 +61,25 @@ class Catalog
  end
 
 
-  def print_sorted_elements(category)         # This method sorts all elements by different categories
-     if category == 1
-       sorted_array = @elements.sort_by{ |element| element.name.to_s }
-     elsif category == 2
-       sorted_array = @elements.sort_by{ |element| element.symbol.to_s }
-     elsif category == 3 
-       sorted_array = @elements.sort_by{ |element| element.num.to_i }
-     elsif category == 4 
-       sorted_array = @elements.sort_by{ |element| element.weight.to_f }
-     elsif category == 5 
-       sorted_array = @elements.sort_by{ |element| element.state.to_s }
-     elsif category == 6 
-       sorted_array = @elements.sort_by{ |element| element.type.to_s }
-     end
-       sorted_array.each do |elem|
-              puts elem.to_s
-        end
-  end
-
+ def print_sorted_elements(category)        
+      if category == 1
+          sorted_array = @elements.sort_by{|element| element.name.to_s}
+      elsif category == 2 
+          sorted_array = @elements.sort_by{|element| element.symbol.to_s}
+      elsif category == 3 
+          sorted_array = @elements.sort_by{|element| element.num.to_i}
+      elsif category == 4 
+          sorted_array = @elements.sort_by{|element| element.weight.to_f}
+      elsif category == 5 
+          sorted_array = @elements.sort_by{|element| element.state.to_s}
+      elsif category == 6 
+          sorted_array = @elements.sort_by{|element| element.type.to_s}
+      end    
+      print_titles
+      sorted_array.each do |item|
+            puts item.to_s
+      end
+ end
  def print_element_by_name(elem)
      print_titles
      @elements.each do |element|
@@ -131,32 +126,28 @@ class Catalog
       end
  end
 
- def add_elem (name, symbol, num, weight, state, type)
+ def add_elem (name, symbol, atomic_num, atomic_weight, state, type)
       CSV.open('elements.txt', 'a') do |csv|
-           csv << [name, symbol, num, weight, state, type]
+           csv << [name, symbol, atomic_num, atomic_weight, state, type]
       end
  end
+ def update_elem(original, change_name, change_symbol, change_num, change_weight, change_state, change_type)
+    @elements.each do |element|
+      if original == element.name
+        element.name = change_name
+        element.symbol = change_symbol
+        element.num = change_num
+        element.weight = change_weight
+        element.state = change_state
+        element.type = change_type
+      end
+    end
 
- def update_elem(original, changed_name, changed_symbol, changed_num, changed_weight, changed_state, changed_type)
-       @elements.each do |element|
-           if original == element.name
-              element.name = changed_name
-              element.symbol = changed_symbol
-              element.num = changed_num
-              element.weight = changed_weight
-              element.state = changed_state
-              element.type = changed_type
-            end
-        end
-
-      #  binding.pry
-        CSV.open('elements.txt', 'w') do |csv|
-
-     #   binding.pry
-           @elements.each do |element|
-                  csv << element.to_array 
-            end
-        end
+       CSV.open('elements.txt', 'w') do |csv|
+          @elements.each do |element|
+             csv << [element.name, element.symbol, element.num, element.weight, element.state, element.type]
+          end
+       end
  end
 
 end                  # End of class Catalog
@@ -166,46 +157,46 @@ end                  # End of class Catalog
 elmnt = Catalog.new
 if ARGV[0] == "all"
     elmnt.print_elements
-end
-if ARGV[0] == "search_by_name" 
+
+elsif ARGV[0] == "search_by_name" 
     elmnt.print_element_by_name(ARGV[1])
-end
-if ARGV[0] == "search_by_symbol"
+
+elsif ARGV[0] == "search_by_symbol"
     elmnt.print_elements_by_type(ARGV[1], 1)
-end
-if ARGV[0] == "search_by_atomic_num"
+
+elsif ARGV[0] == "search_by_atomic_num"
     elmnt.print_element_by_atomic_num(ARGV[1])
-end
-if ARGV[0] == "search_by_atomic_weight"
+
+elsif ARGV[0] == "search_by_atomic_weight"
     elmnt.print_elements_by_AtomicWeight(ARGV[1], ARGV[2])
-end
-if ARGV[0] == "search_by_state" 
+
+elsif ARGV[0] == "search_by_state" 
     elmnt.print_elements_by_state(ARGV[1])
-end
-if ARGV[0] == "search_by_type"
+
+elsif ARGV[0] == "search_by_type"
     elmnt.print_elements_by_type(ARGV[1])
-end
-if ARGV[0] == "add_element"
+
+elsif ARGV[0] == "add_element"
      elmnt.add_elem(ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5], ARGV[6])
-end
-if ARGV[0] == "update_element"
+
+elsif ARGV[0] == "update_element"
      elmnt.update_elem(ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5], ARGV[6], ARGV[7])
-end
-if ARGV[0] == "sort_by_name"
+
+elsif ARGV[0] == "sort_by_name"
      elmnt.print_sorted_elements(1)
-end
-if ARGV[0] == "sort_by_symbol"
+
+elsif ARGV[0] == "sort_by_symbol"
      elmnt.print_sorted_elements(2)
-end
-if ARGV[0] == "sort_by_atomic_num"
+
+elsif ARGV[0] == "sort_by_atomic_num"
      elmnt.print_sorted_elements(3)
-end
-if ARGV[0] == "sort_by_atomic_weight"
+
+elsif ARGV[0] == "sort_by_atomic_weight"
      elmnt.print_sorted_elements(4)
-end
-if ARGV[0] == "sort_by_state"
+
+elsif ARGV[0] == "sort_by_state"
      elmnt.print_sorted_elements(5)
-end
-if ARGV[0] == "sort_by_type"
+
+elsif ARGV[0] == "sort_by_type"
      elmnt.print_sorted_elements(6)
 end
